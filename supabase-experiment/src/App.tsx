25175fa12c5bb2e4b7e489dd42d4ab593a882a9d
Shu Tonoike <shu.tonoike@genialtech.io>
2024-10-08 00:03:12 +0900

    wip

--- supabase-experiment/src/App.tsx
+++ supabase-experiment/src/App.tsx
@@ -1,36 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
-import {client} from "./supabse"
+import { client } from "./supabse"
+import { RealtimeChannel } from '@supabase/supabase-js'
+import { Player } from './types'
+
+let initilized = false
+
+const copyToClipboard = (text: string) => {
+  navigator.clipboard.writeText(text)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  console.log(client)
+  const [roomId, setRoomId] = useState<string | undefined>(undefined)
+  const [channel, setChannel] = useState<RealtimeChannel | undefined>(undefined)
+  const [player, setPlayer] = useState<Player | undefined>(undefined)
+  const [players, setPlayers] = useState<Player[]>([])
+
+  const [initializeStatus, setInitializeStatus] = useState<"initializing" | "failed" | "ok">("initializing")
+
+  useEffect(() => {
+    if (channel || initilized) return
+
+    initilized = true
+
+    const url = new URL(window.location.href)
+    const roomIdFromParam = url.searchParams.get('room_id')
+    const isHost = Boolean(roomIdFromParam)
+
+    const roomId = roomIdFromParam || crypto.randomUUID()
+    setRoomId(roomId)
+
+    const roomOne = client.channel(roomId)
+    setChannel(roomOne)
+
+
+    let playerName = window.prompt('プレイヤー名を入力してください')
+    while (!playerName || !playerName.trim()) {
+      playerName = window.prompt('プレイヤー名は必須です')
+    }
+    const player = { id: crypto.randomUUID(), name: playerName, isHost }
+    setPlayer(player)
+
+    roomOne
+      .on('presence', { event: 'join' }, ({ newPresences }) => {
+        const joinedPlayer = newPresences.map((presence) => presence.data)
+        setPlayers((prevPlayers) => [...prevPlayers, ...joinedPlayer])
+      })
+      .on('presence', { event: 'leave' }, ({ leftPresences }) => {
+        setPlayers((prevPlayers) => prevPlayers.filter((player) => !leftPresences.some((presence) => presence.data.id === player.id)))
+      })
+      .subscribe(async (status) => {
+        if (status !== 'SUBSCRIBED') {
+          console.error('Failed to subscribe', status)
+          return
+        }
+
+        const presenceTrackStatus = await roomOne.track({ data: player })
+        if (presenceTrackStatus !== "ok") {
+          console.error('Failed to track', presenceTrackStatus)
+
+        }
+      })
+
+
+
+  }, [])
+
+
+  if(initializeStatus === "initializing"){
+    return <span className="loading loading-dots loading-lg"></span>
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>supabase url is {} </p>
+     <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <p>{`${window.location.origin}?room_id=${roomId}`}<button onClick={() => copyToClipboard(`${window.location.origin}?room_id=${roomId}`)}>Copy</button></p>
+      <h3>参加中のプレイヤー</h3>
+      {players.map((player) => (
+        <p key={player.id}>{player.name}</p>
+      ))}
     </>
   )
 }
--- supabase-experiment/src/main.tsx
+++ supabase-experiment/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+      <div className="flex justify-center items-center min-h-screen">
+        <App />
+      </div>
+    </StrictMode>
 )
